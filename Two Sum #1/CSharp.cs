9edf916252d4113b85ee1cd5c74124871b72bf49
William-M3 <WilliamTruMurphy@gmail.com>
2025-04-28 17:18:23 -0400

    Two Sum Problem  in CSharp in O(n)

--- Two Sum #1/CSharp.cs
+++ Two Sum #1/CSharp.cs
@@ -13,3 +13,25 @@ public class Solution {
         return [0,0];
     }
 }
+
+// Second Answer is O(n)
+// Uses a 1-way hash table to solve the problem
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int,int> map = new Dictionary<int,int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if(map.ContainsKey(complement) && map[complement] != i)
+            {
+                return [i, map[complement]];
+            }
+            else
+            {
+                map[nums[i]] = i ;     
+            }
+        }
+        return [0,0];
+    }
+}

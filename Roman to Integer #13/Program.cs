e6ef2714098e22bd66989df61c9f001915fc59c2
William-M3 <WilliamTruMurphy@gmail.com>
2025-05-07 22:23:55 -0400

    Roman To Integer #13 in C# in O(n)

---
+++ Roman to Integer #13/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.WriteLine(RomanToInt("IV"));
+
+int RomanToInt(string s)
+{
+    Dictionary<char, int> valueMap = new Dictionary<char, int>
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000},
+
+    };
+    int result = 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+
+        if (i + 1 < s.Length)
+        {
+            if (valueMap[s[i]] < valueMap[s[i + 1]])
+            {
+                result += valueMap[s[i + 1]] - valueMap[s[i]];
+                i++;
+                continue;
+            }
+        }
+        result += valueMap[s[i]];
+    }
+    return result;
+
+}

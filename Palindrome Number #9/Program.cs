3df0842205ecad723caeb3befa50839c985d4fa5
William-M3 <WilliamTruMurphy@gmail.com>
2025-05-07 20:41:42 -0400

    Palindrome Number #9 in C# in O(n)

---
+++ Palindrome Number #9/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine(IsPalindrome(10));
+
+bool IsPalindrome (int x)
+{
+    
+    if (x < 0)
+    {
+        return false;
+    }
+
+    int reverse = 0;
+    int temp = x;
+
+    while (temp != 0)
+    {
+        reverse = (reverse * 10) + (temp % 10);
+        temp /= 10;
+    }
+    return reverse == x;
+}

c1ebcb9aab423796021f639b6af717ae8be165d5
William-M3 <WilliamTruMurphy@gmail.com>
2025-05-08 14:09:34 -0400

    Longest Common Prefix #14 in C# in O(n^2)

---
+++ Longest Common Prefix #14/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string LongestCommonPrefix(string[] strs)
+{
+    string prefix = "";
+    Array.Sort(strs);
+    for (int i = 0; i < strs[0].Length; i++)
+    {
+        for (int j = 1; j < strs.Length; j++)
+        {
+            if (strs[0][i] != strs[j][i])
+                return prefix;
+        }
+        prefix += strs[0][i];
+    }
+    return prefix;
+}

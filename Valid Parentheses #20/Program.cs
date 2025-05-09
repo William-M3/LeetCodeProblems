ddca8c479d03acc035d5fdfba9a61fc5861691ff
William-M3 <WilliamTruMurphy@gmail.com>
2025-05-09 14:24:16 -0400

    Rewritten for cleaner code.

--- Valid Parentheses #20/Program.cs
+++ Valid Parentheses #20/Program.cs
@@ -1,47 +1,82 @@
-﻿public class Solution {
-    public bool IsValid(string s) {
-        
-        
-    if (s.Length % 2 == 1)
+﻿
+
+
+// public class Solution {
+//     public bool IsValid(string s) {
+
+//     if (s.Length % 2 == 1 || !(s[i] == '(' || s[i] == '{' || s[i] == '['))
+//     {
+//         return false;
+//     }
+
+//     Dictionary<char, char> hashMap = new Dictionary<char, char> 
+//     {
+//         {'(', ')'},
+//         {'{', '}'},
+//         {'[', ']'}
+//     };
+//     Stack<char> input = new Stack<char>();
+
+
+//     for (int i = 0; i < s.Length; i++)
+//     {
+//         if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+//         {
+//             input.Push(s[i]);
+//         }
+//         else
+//         {
+//             if(input.Count == 0)
+//             {
+//                 return false;
+//             }
+
+//             if(!(hashMap[input.Pop()] == s[i]))
+//             {
+//                 return false;
+//             }
+
+//         }
+//     }
+
+//     if (input.Count != 0)
+//     {
+//         return false;
+//     }
+
+//     return true;
+//     }
+// }
+
+
+public class Solution
+{
+    public bool IsValid(string s)
     {
-        return false;
-    }
-    
-    Dictionary<char, char> hashMap = new Dictionary<char, char> 
+        Dictionary<char, char> hashMap = new Dictionary<char, char>
     {
         {'(', ')'},
         {'{', '}'},
         {'[', ']'}
     };
-    Stack<char> input = new Stack<char>();
-    
-    
-    for (int i = 0; i < s.Length; i++)
-    {
-        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        Stack<char> input = new Stack<char>();
+
+
+        for (int i = 0; i < s.Length; i++)
         {
-            input.Push(s[i]);
-        }
-        else
-        {
-            if(input.Count == 0)
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                input.Push(s[i]);
+            }
+            else if (input.Count > 0 && s[i] == hashMap[input.Peek()])
+            {
+                input.Pop();
+            }
+            else
             {
                 return false;
             }
-
-            if(!(hashMap[input.Pop()] == s[i]))
-            {
-                return false;
-            }
-
         }
-    }
-
-    if (input.Count != 0)
-    {
-        return false;
-    }
-
-    return true;
+        return input.Count == 0;
     }
 }

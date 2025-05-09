1046829df1140c2fdb7d15a347a1ee0707fbba5e
William-M3 <WilliamTruMurphy@gmail.com>
2025-05-09 12:57:45 -0400

    Container With Most Water #11

---
+++ Container With Most Water #11/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine(MaxArea([1, 1]));
+
+int MaxArea(int[] height)
+{
+    int MaxArea = 0;
+    int currentArea;
+    unsafe
+    {
+        fixed (int* p1 = &height[0])
+        {
+            fixed (int* p2 = &height[height.Length - 1])
+            {
+                int* ptrStart = p1;
+                int* ptrFinal = p2;
+                for (int i = 0; i < height.Length; i++)
+                {
+                    if(*ptrStart > *ptrFinal)
+                    {
+                        currentArea = *ptrFinal * (height.Length - 1 - i);
+                        ptrFinal--;
+                    }
+                    else
+                    {
+                       currentArea = *ptrStart * (height.Length - 1 - i);
+                       ptrStart++;
+                    }
+                    if (MaxArea < currentArea)
+                    {
+                        MaxArea = currentArea;
+                    }
+                }
+            }
+        }
+    }
+    return MaxArea;
+}

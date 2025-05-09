b9dd3ec2f29e2db7767a5039bcb9cec29f722475
William-M3 <WilliamTruMurphy@gmail.com>
2025-05-09 19:54:44 -0400

    Reverse Integer, log (n), C#, not robust

---
+++ Reverse Integer #7/Program.cs
@@ -0,0 +1,20 @@
+﻿Reverse(-123);
+
+Console.WriteLine(Int32.MaxValue);
+
+
+int Reverse(int x)
+{
+
+    int reverseNumber = 0;
+
+    while (x != 0)
+    {
+        reverseNumber = (reverseNumber * 10) + (x % 10);
+        if (reverseNumber % 10 != x % 10)
+            return 0;
+        x = x / 10;
+    }
+
+    return reverseNumber;
+}
